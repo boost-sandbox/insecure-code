826941e60c677dad16256ec82256afa1e81de3e1
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2022-07-04 15:54:08 -0400

    Update github.py

--- canaries/secrets/github.py
+++ canaries/secrets/github.py
@@ -1,4 +1,5 @@
 
 import requests
-r = requests.get('https://github.com/token', auth=('user', 'ghp_c9eSqebJMCbYoGf218TqGz80BXnfku2hRyg9'))
+
+r = aaaarequests.get('https://github.com/token', auth=('user', 'ghp_c9eSqebJMCbYoGf218TqGz80BXnfku2hRyg7')) 79878
 r.status_code
