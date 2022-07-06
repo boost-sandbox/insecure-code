f1c39712ff5c8e6ed184231ccfb695ce9f5f46d2
fpboostdemo <108748911+fpboostdemo@users.noreply.github.com>
2022-07-06 12:08:31 -0400

    Update hardcoded-jwt.rb

--- canaries/ruby/hardcoded-jwt.rb
+++ canaries/ruby/hardcoded-jwt.rb
@@ -4,6 +4,7 @@ class Something
    CLOSE_YOUR_EYES = "uyufidsy768rew786erwuyiuyrwe"
 
    def generate_jwt(payload)
+      JWT.encode payload, 'this-is-a-secret', 'HS256'
       return JWT.encode payload, CLOSE_YOUR_EYES, 'HS256'
    end
 end
